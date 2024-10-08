3789c1c62dcbe6d6f4d25c4e0142e394c4f2872f
ElvynM <elvynmartins30@gmail.com>
2024-10-08 19:14:37 -0300

    sistema de estaciomamento

---
+++ Estacionamento/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n");
+
+Console.WriteLine("Digite o preço inicial:");
+decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o preço por hora:");
+decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento.Models.Estacionamento estacionamento = new Estacionamento.Models.Estacionamento(precoInicial, precoPorHora);
+
+
+bool continuar = true;
+while (continuar)
+{
+    Console.WriteLine("\nDigite a sua opção:");
+    Console.WriteLine("1 - Adicionar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    string opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            estacionamento.AdicionarVeiculo();
+            break;
+        case "2":
+            estacionamento.RemoverVeiculo();
+            break;
+        case "3":
+            estacionamento.ListarVeiculos();
+            break;
+        case "4":
+            continuar = false;
+            Console.WriteLine("Encerrando o programa...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+}
